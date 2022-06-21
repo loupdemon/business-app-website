0374f2143dc4562008720b8efb359ec7fa169ebb
Yurii <94929434+Yurii-Sapsai@users.noreply.github.com>
2022-06-21 18:28:10 +0300

    add new component - Navbar

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,46 @@
+import React, { FC, useState } from 'react'
+import './Navbar.sass'
+
+import {FaBars, FaTimes} from 'react-icons/fa'
+
+
+
+const Logo =  require("../../images/logo.png")
+
+const Navbar:FC = () => {
+
+    const [click, setClick] = useState<boolean>(false)
+
+    const handleClick = () => {
+        setClick(!click)
+    }
+
+  return (
+    <header className='header'>
+        <nav className='navbar'>
+            <a href="/" className='logo'>
+                <img src={Logo} alt="logo" />
+            </a>
+            <div className='humburger' onClick={handleClick}>
+                {click ? (<FaTimes size={30} style={{color:'white'}}/>) :  (<FaBars size={30} style={{color:'white'}}/>)}
+            </div>
+            <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                <li className='nav-item'>
+                    <a href="">Home</a>
+                </li>
+                <li className='nav-item'>
+                    <a href="">About</a>
+                </li>
+                <li className='nav-item'>
+                    <a href="">Testimonials</a>
+                </li>
+                <li className='nav-item'>
+                    <a href="">Demo</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
+
+export default Navbar
